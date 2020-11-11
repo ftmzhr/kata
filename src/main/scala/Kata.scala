c437180a90b53ca218b3c2c184242bab0ca8c8cc
fatimazahramounaji <fatimazahra.mounaji@gmail.com>
2020-11-11 19:59:20 +0100

    alphabetPosition use mkstring instead of foldLeft and compare with solution using filter+map

--- src/main/scala/Kata.scala
+++ src/main/scala/Kata.scala
@@ -1,14 +1,20 @@
+
+
 object Kata {
 
+  //iterate only once and use mkstring
   def alphabetPosition(text: String): String = {
     if(!(text == null || text.isEmpty))
-      text.toLowerCase
-        .flatMap(char => char.isLetter match {
+      text.flatMap(char => char.toLower.isLetter match {
           case true => Some(char.toInt - 96)
           case false => None
-        }).foldLeft(""){(acc,num) => acc.concat(num.toString).concat(" ")}.replaceAll("\\s+$", "")
+        }).mkString(" ")
     else ""
   }
+
+  // iterate twice : filter, map
+  def alphabetPosition2(text: String): String =
+    text.filter(_.isLetter).map(_.toLower - 96).mkString(" ")
 
   def main(args: Array[String]): Unit = {
     print(alphabetPosition("abcd9l"))

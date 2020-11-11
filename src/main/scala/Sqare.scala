90d5825d808e82b11b2ba6fe7e22a276d19b3e97
fatimazahramounaji <fatimazahra.mounaji@gmail.com>
2020-11-11 20:42:21 +0100

    use math lib

--- src/main/scala/Sqare.scala
+++ src/main/scala/Sqare.scala
@@ -3,13 +3,7 @@
 object Sqare {
 
     def isSquare(x: Int): Boolean = {
-      if( x == 0) true
-      else {
-        val divisors = Seq.range(1,x/2+1)
-        if(divisors.exists(elem => (elem*elem).equals(x))) true
-        else
-          false
-      }
+      math.sqrt(x).isWhole
     }
 
   def main(args: Array[String]): Unit = {
